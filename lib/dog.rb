8f01cf0dc4b8227ae62da477cdbef42cd9946c84
Allan Marquez <allan.marquez.2003@icloud.com>
2022-12-14 18:56:25 -0700

    commit tree e3d94ad551c7ebf858d1679264f3f04a70ffe68d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name 
+    end 
+    def name 
+        @this_dogs_name
+    end 
 end
+
+max = Dog.new
+max.name ="max"
+
+puts max.name 
+
